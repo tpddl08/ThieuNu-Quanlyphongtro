01e02fefe82eb4573ea23d08eb2436ac2c84effe
tpddl08 <tpddl08@gmail.com>
2026-03-27 19:39:30 +0700

    update

--- QLPT/WinFormsApp1/WinFormsApp1/frmCreateroom.Designer.cs
+++ QLPT/WinFormsApp1/WinFormsApp1/frmCreateroom.Designer.cs
@@ -30,23 +30,18 @@
         {
             label1 = new Label();
             panel1 = new Panel();
+            btnSubmit = new Button();
+            txtService = new TextBox();
             label4 = new Label();
             label2 = new Label();
             txtNumbermember = new TextBox();
             txtNameroom = new TextBox();
-            txtService = new TextBox();
-            txtWatter = new TextBox();
-            txtElectric = new TextBox();
             label5 = new Label();
-            label6 = new Label();
-            lb7 = new Label();
-            btnSubmit = new Button();
             tableLayoutPanel1 = new TableLayoutPanel();
             panel2 = new Panel();
             panel3 = new Panel();
             panel1.SuspendLayout();
             tableLayoutPanel1.SuspendLayout();
-            panel3.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -56,7 +51,7 @@
             label1.Font = new Font("Segoe UI", 26F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.Location = new Point(96, 0);
             label1.Name = "label1";
-            label1.Size = new Size(369, 96);
+            label1.Size = new Size(744, 96);
             label1.TabIndex = 6;
             label1.Text = "TẠO PHÒNG";
             label1.TextAlign = ContentAlignment.MiddleLeft;
@@ -64,6 +59,7 @@
             // panel1
             // 
             panel1.BackColor = SystemColors.GradientActiveCaption;
+            panel1.Controls.Add(btnSubmit);
             panel1.Controls.Add(txtService);
             panel1.Controls.Add(label4);
             panel1.Controls.Add(label2);
@@ -73,8 +69,32 @@
             panel1.Dock = DockStyle.Fill;
             panel1.Location = new Point(96, 99);
             panel1.Name = "panel1";
-            panel1.Size = new Size(369, 284);
+            panel1.Size = new Size(744, 284);
             panel1.TabIndex = 5;
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnSubmit.BackColor = Color.ForestGreen;
+            btnSubmit.FlatStyle = FlatStyle.Flat;
+            btnSubmit.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSubmit.ForeColor = SystemColors.ActiveCaptionText;
+            btnSubmit.Location = new Point(474, 237);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(252, 44);
+            btnSubmit.TabIndex = 4;
+            btnSubmit.Text = "Tạo";
+            btnSubmit.UseVisualStyleBackColor = false;
+            btnSubmit.Click += btnSubmit_Click;
+            // 
+            // txtService
+            // 
+            txtService.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtService.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtService.Location = new Point(209, 170);
+            txtService.Name = "txtService";
+            txtService.Size = new Size(517, 50);
+            txtService.TabIndex = 16;
             // 
             // label4
             // 
@@ -106,7 +126,7 @@
             txtNumbermember.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
             txtNumbermember.Location = new Point(244, 92);
             txtNumbermember.Name = "txtNumbermember";
-            txtNumbermember.Size = new Size(107, 50);
+            txtNumbermember.Size = new Size(482, 50);
             txtNumbermember.TabIndex = 7;
             // 
             // txtNameroom
@@ -115,35 +135,8 @@
             txtNameroom.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
             txtNameroom.Location = new Point(219, 16);
             txtNameroom.Name = "txtNameroom";
-            txtNameroom.Size = new Size(132, 50);
+            txtNameroom.Size = new Size(507, 50);
             txtNameroom.TabIndex = 5;
-            // 
-            // txtService
-            // 
-            txtService.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            txtService.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            txtService.Location = new Point(209, 170);
-            txtService.Name = "txtService";
-            txtService.Size = new Size(142, 50);
-            txtService.TabIndex = 16;
-            // 
-            // txtWatter
-            // 
-            txtWatter.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            txtWatter.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            txtWatter.Location = new Point(201, 92);
-            txtWatter.Name = "txtWatter";
-            txtWatter.Size = new Size(142, 50);
-            txtWatter.TabIndex = 15;
-            // 
-            // txtElectric
-            // 
-            txtElectric.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            txtElectric.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            txtElectric.Location = new Point(201, 16);
-            txtElectric.Name = "txtElectric";
-            txtElectric.Size = new Size(142, 50);
-            txtElectric.TabIndex = 14;
             // 
             // label5
             // 
@@ -157,52 +150,13 @@
             label5.TabIndex = 13;
             label5.Text = "Phí nhà ở";
             // 
-            // label6
-            // 
-            label6.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
-            label6.ForeColor = Color.Black;
-            label6.Location = new Point(14, 95);
-            label6.Name = "label6";
-            label6.Size = new Size(168, 45);
-            label6.TabIndex = 12;
-            label6.Text = "Tiền nước";
-            // 
-            // lb7
-            // 
-            lb7.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            lb7.AutoSize = true;
-            lb7.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
-            lb7.ForeColor = Color.Black;
-            lb7.Location = new Point(14, 16);
-            lb7.Name = "lb7";
-            lb7.Size = new Size(158, 45);
-            lb7.TabIndex = 11;
-            lb7.Text = "Tiền điện";
-            // 
-            // btnSubmit
-            // 
-            btnSubmit.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            btnSubmit.BackColor = Color.ForestGreen;
-            btnSubmit.FlatStyle = FlatStyle.Flat;
-            btnSubmit.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnSubmit.ForeColor = SystemColors.ActiveCaptionText;
-            btnSubmit.Location = new Point(233, 176);
-            btnSubmit.Name = "btnSubmit";
-            btnSubmit.Size = new Size(110, 44);
-            btnSubmit.TabIndex = 4;
-            btnSubmit.Text = "Tạo";
-            btnSubmit.UseVisualStyleBackColor = false;
-            btnSubmit.Click += btnSubmit_Click;
-            // 
             // tableLayoutPanel1
             // 
             tableLayoutPanel1.AutoSize = true;
             tableLayoutPanel1.ColumnCount = 4;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 40F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 40F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 80F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 0F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
             tableLayoutPanel1.Controls.Add(panel2, 0, 0);
             tableLayoutPanel1.Controls.Add(label1, 1, 0);
@@ -228,15 +182,10 @@
             // panel3
             // 
             panel3.BackColor = SystemColors.GradientActiveCaption;
-            panel3.Controls.Add(lb7);
-            panel3.Controls.Add(txtWatter);
-            panel3.Controls.Add(btnSubmit);
-            panel3.Controls.Add(txtElectric);
-            panel3.Controls.Add(label6);
             panel3.Dock = DockStyle.Fill;
-            panel3.Location = new Point(471, 99);
+            panel3.Location = new Point(846, 99);
             panel3.Name = "panel3";
-            panel3.Size = new Size(369, 284);
+            panel3.Size = new Size(1, 284);
             panel3.TabIndex = 7;
             // 
             // frmCreateroom
@@ -254,8 +203,6 @@
             panel1.PerformLayout();
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
-            panel3.ResumeLayout(false);
-            panel3.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -270,11 +217,7 @@
         private TextBox txtNameroom;
         private Button btnSubmit;
         private Label label5;
-        private Label label6;
-        private Label lb7;
         private TextBox txtService;
-        private TextBox txtWatter;
-        private TextBox txtElectric;
         private TableLayoutPanel tableLayoutPanel1;
         private Panel panel2;
         private Panel panel3;
